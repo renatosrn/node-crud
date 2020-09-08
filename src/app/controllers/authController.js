d9d69c5297ac7904a0945277fa1ba44197ab275a
Renato de Souza <renato.neto@sensedia.com>
2020-09-08 14:10:45 -0300

    Adicionada nova rota de reset password

--- src/app/controllers/authController.js
+++ src/app/controllers/authController.js
@@ -94,4 +94,32 @@ router.post('/forgot_password', async (req, res) => {
     }
 });
 
+router.post('/reset_password', async (req, res) => {
+    const { email, token, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email })
+            .select('+passwordResetToken passwordResetExpires');
+
+        if (!user)
+            return res.status(400).send({ error: 'User not found' });
+
+        if (token !== user.passwordResetToken)
+            return res.status(400).send({ error: 'Token invalid' });
+
+        const now = new Date();
+
+        if (now > user.passwordResetExpires)
+            return res.status(400).send({ error: 'Token  expired, generate a new one' });
+
+        user.password = password;
+
+        await user.save();
+
+        res.send();
+    } catch (err) {
+        res.status(400).send({ error: 'Cannot reset password, try again' });
+    }
+});
+
 module.exports = app => app.use('/auth', router);
